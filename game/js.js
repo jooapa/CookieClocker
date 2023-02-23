2aadc1c2bbc417fb2f3a2732aa4fce4f6c78d0dc
jooapa <37744313+jooapa@users.noreply.github.com>
2023-02-23 13:34:10 +0200

    start of

---
+++ game/js.js
@@ -0,0 +1,35 @@
+cakeLevel = 1;
+class Maincake{    
+    constructor(cakes, multiplyCake, cakePrice){
+    this.cakes = cakes;
+    this.multiplyCake = multiplyCake;
+    this.cakePrice = cakePrice;
+    }  
+    giveCake(){
+        this.cakes += 1 * this.multiplyCake;
+        this.näytäTiedot();
+    }
+
+    upgradeCake(){
+        if(this.cakes >= this.cakePrice){
+            this.cakes -= this.cakePrice
+            this.cakePrice *= 2
+            this.multiplyCake += 1
+            this.näytäTiedot()
+        }
+    }
+
+    näytäTiedot(){
+        document.getElementById("tiedot").innerHTML = "Cakes: " + this.cakes;
+        document.getElementById("tiedot2").innerHTML = "Cake Multiplier: " + this.multiplyCake;
+        document.getElementById("tiedot3").innerHTML = "Cake Upgrade Price: " + this.cakePrice;
+    }
+
+    
+}
+
+function improveCake(){
+    
+}
+
+let cake1 = new Maincake(0, 1, 10);
