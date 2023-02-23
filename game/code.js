225604d4fcc8cd38efa74dd789ded7e98b67ff88
jooapa <jooa.akonpelto@gmail.com>
2023-02-23 22:52:37 +0200

    Prototype at ready

---
+++ game/code.js
@@ -0,0 +1,76 @@
+let currentDroppable = null;
+var Inside = false;
+
+cake.onmousedown = function(event) {
+    Inside = false;
+  let shiftX = event.clientX - cake.getBoundingClientRect().left;
+  let shiftY = event.clientY - cake.getBoundingClientRect().top;
+
+    cake.style.position = 'absolute';
+    cake.style.zIndex = 1000;
+    document.body.append(cake);
+
+    moveAt(event.pageX, event.pageY);
+
+    function moveAt(pageX, pageY) {
+        cake.style.left = pageX - shiftX + 'px';
+        cake.style.top = pageY - shiftY + 'px';
+    }
+
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+
+    cake.hidden = true;
+    let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+    cake.hidden = false;
+
+    if (!elemBelow) return;
+
+    let droppableBelow = elemBelow.closest('.droppable');
+    if (currentDroppable != droppableBelow) {
+      if (currentDroppable) { // null when we were not over a droppable before this event
+        leaveDroppable(currentDroppable, cake);
+        Inside = false;
+        console.log(Inside)
+      }
+      currentDroppable = droppableBelow;
+      if (currentDroppable) { // null if we're not coming over a droppable now
+        // (maybe just left the droppable)
+        enterDroppable(currentDroppable, cake);
+        Inside = true;
+        console.log(Inside)
+      }
+    }
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+
+  cake.onmouseup = function() {
+    if(Inside == true){
+        console.log("syöty")
+    }
+    else{
+        console.log("ei syöty")
+    }
+    document.removeEventListener('mousemove', onMouseMove);
+    cake.onmouseup = null;
+    cake.style.top = 0 
+    cake.style.left = 0
+  };
+
+};
+
+function enterDroppable(elem, obj) {
+  elem.style.background = 'pink';
+  
+}
+
+function leaveDroppable(elem, obj) {
+  elem.style.background = '';
+  
+}
+
+cake.ondragstart = function() {
+  return false;
+};
+
